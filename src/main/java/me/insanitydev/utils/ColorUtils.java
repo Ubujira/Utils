1bf5ead4b2b60b1f4ddb1f6a17f311740de45403
InsanityDev <realInsanityDeveloper@gmail.com>
2021-09-16 22:48:47 +0200

    Initial commit

---
+++ src/main/java/me/insanitydev/utils/ColorUtils.java
@@ -0,0 +1,32 @@
+package me.insanitydev.utils;
+
+import java.awt.*;
+import java.util.Random;
+
+public class ColorUtils {
+
+    /**
+     * Creates a random color with additional brightness.
+     * @param brightnessIncrease Defines how much brighter the color is.
+     * @return The random color.
+     */
+    public static Color randomColor(float brightnessIncrease) {
+        Random random = new Random();
+
+        float red = random.nextFloat() + brightnessIncrease;
+        float green = random.nextFloat() + brightnessIncrease;
+        float blue = random.nextFloat() + brightnessIncrease;
+
+        return new Color(red, green , blue);
+    }
+
+    /**
+     * Creates a random color with no additional brightness.
+     * @return The random color.
+     */
+    public static Color randomColor() {
+        Random random = new Random();
+        return new Color(random.nextFloat(), random.nextFloat() , random.nextFloat());
+    }
+
+}
---
+++ src/main/java/me/insanitydev/utils/JsonUtils.java
@@ -0,0 +1,35 @@
+package me.insanitydev.utils;
+
+import com.google.gson.*;
+
+import java.io.*;
+
+public class JsonUtils {
+
+    /**
+     * Reads json from a file.
+     * @param file The file to read from.
+     * @return The Json element.
+     * @throws FileNotFoundException If the file doesn't exist.
+     */
+    public static JsonElement readJsonFromFile(File file) throws FileNotFoundException {
+        if (file.exists()) {
+            return JsonParser.parseReader(new FileReader(file));
+        }
+        return null;
+    }
+
+    /**
+     * Writes a Json object to a file.
+     * @param file The file to write to.
+     * @param jsonObject The json object to write to the file.
+     * @throws IOException If the file can't be written to.
+     */
+    public static void writeJsonObjectToFile(File file, JsonObject jsonObject) throws IOException {
+        try (Writer writer = new FileWriter(file)) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(jsonObject, writer);
+        }
+    }
+
+}
