f69fd24cc3e77c8632e764c92ed5d8011ce12232
InsanityDev <realInsanityDeveloper@gmail.com>
2021-09-18 12:05:20 +0200

    Add math utils

---
+++ src/main/java/me/insanitydev/utils/MathUtils.java
@@ -0,0 +1,29 @@
+package me.insanitydev.utils;
+
+public class MathUtils {
+
+    /**
+     * Makes sure that the provided integer is within a defined range.
+     *
+     * @param value The integer to check.
+     * @param min   The minimum value it must be.
+     * @param max   The maximum value it can be.
+     * @return The same value or a value at minimum or maximum.
+     */
+    public static int ensureRange(int value, int min, int max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    /**
+     * Checks if the integer is within the defined range.
+     *
+     * @param value The integer to check.
+     * @param min   The minimum value.
+     * @param max   The maximum value.
+     * @return Whether the value is between the minimum and maximum value.
+     */
+    public static boolean isInRange(int value, int min, int max) {
+        return (value <= max) && (value >= min);
+    }
+
+}
